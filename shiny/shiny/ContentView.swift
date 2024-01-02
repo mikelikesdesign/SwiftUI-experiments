8c324a450fefed4913722e794f558cb21dce4414
Michael <mike@ricecakesmedia.com>
2024-01-02 09:42:13 +0900

    shiny effect

---
+++ shiny/shiny/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  shiny
+//
+//  Created by Michael Lee on 1/2/24.
+//
+
+import SwiftUI
+import Shimmer
+
+struct ContentView: View {
+    var body: some View {
+        Text("Happy new year, yall")
+            .font(.title)
+            .shimmering()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ shiny/shiny/shinyApp.swift
@@ -0,0 +1,17 @@
+//
+//  shinyApp.swift
+//  shiny
+//
+//  Created by Michael Lee on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct shinyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
