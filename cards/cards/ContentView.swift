fad0c6bddb3acf07e213903f7f009168a59de361
Michael <mike@ricecakesmedia.com>
2024-02-11 23:15:13 +0900

    updates

---
+++ cards/cards/ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  cards
+//
+//  Created by Michael Lee on 11/5/23.
+//
+
+//
+//  ContentView.swift
+//  cards
+//
+//  Created by Michael Lee on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let emojis =  ["🏀","⚽️","🥎","🏓", "🏆", "😀", "🍟", "🍣", "✏️", "🌴", "🐶", "🦆"]
+    
+    @State var cardCount: Int = 4
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+            
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cards/cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by Michael Lee on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
